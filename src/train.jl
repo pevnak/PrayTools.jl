31f36e3a46ada6e021e4a204463c518d3954bcc0
Pevnak <pevnak@gmail.com>
2021-01-20 21:02:45 +0100

    added trainy

--- src/train.jl
+++ src/train.jl
@@ -12,3 +12,18 @@ function train!(loss, ps, preparesamples, opt, iterations; cb = () -> (), cby = 
   end
 end
 
+"""
+	trainy!(loss, ps, preparesamples, opt, iterations; cb = () -> (), cby = (y) -> ())
+"""
+function trainy!(loss, ps, preparesamples, opt, iterations; cb = () -> (), cby = (y) -> ())
+  ps = Flux.Params(ps)
+  cb = Flux.Optimise.runall(cb)
+  for i in 1:iterations
+	  	x = preparesamples()
+        y, gs = gradient(() -> loss(x...), ps)
+        Flux.Optimise.update!(opt, ps, gs)
+        cb()
+        cby(y)
+  end
+end
+
