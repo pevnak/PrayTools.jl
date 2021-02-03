908c57883a20f494d1502209f187a44b3862d2f8
Pevnak <pevnak@gmail.com>
2021-02-03 19:29:27 +0100

    added support for dropout in ffnn

--- src/ffnn.jl
+++ src/ffnn.jl
@@ -15,13 +15,17 @@ Chain(Dense(3, 5, tanh), Dense(5, 5, tanh))
 julia> PrayTools.ffnn(3, 5, 2, tanh, identity)
 Chain(Dense(3, 5, tanh), BatchNorm(5), Dense(5, 5, tanh), BatchNorm(5))
 """
-function ffnn(idim, nneurons, nlayers, fun, bnfun)
+function ffnn(idim, nneurons, nlayers, fun, bnfun, dr)
 	c = []
 	for i in 1:nlayers
 		idim = i == 1 ? idim : nneurons
 		push!(c, Dense(idim, nneurons, fun))
 		if bnfun != nothing
 			push!(c, BatchNorm(nneurons, bnfun))
+		end
+
+		if dr > 0 
+			push!(c, Flux.Dropout(dr))
 		end
 	end
 	Chain(c...)
